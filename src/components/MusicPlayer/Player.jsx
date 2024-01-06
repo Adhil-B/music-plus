13e26897ba7db9a4303a0f5a8abede9d03fc83da
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-01-07 03:55:14 +0400

    Update Player.jsx

--- src/components/MusicPlayer/Player.jsx
+++ src/components/MusicPlayer/Player.jsx
@@ -72,7 +72,7 @@ const Player = ({ activeSong, isPlaying, volume, seekTime, onEnded, onTimeUpdate
 
   setInterval(function(){
 
- 	if (!ref.current. || ref.current.paused){return;}
+ 	if (!ref.current || ref.current.paused){return;}
 
 	if (!navigator.mediaSession) {return;}
 
