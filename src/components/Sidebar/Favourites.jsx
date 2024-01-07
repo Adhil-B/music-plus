f6fe53bf94f01816241de4c860ec1646901bc3e7
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-01-07 17:44:03 +0400

    Update Favourites.jsx

--- src/components/Sidebar/Favourites.jsx
+++ src/components/Sidebar/Favourites.jsx
@@ -6,8 +6,8 @@ import { AiFillHome } from 'react-icons/ai'
 
 const Favourites = ({setShowNav}) => {
   return (
-    <div className='cside mt-7 border-b border-gray-400 w-[95%] '>
-    <Link href='/favourite' className='hover:text-[#00e6e6] pl-[6px] flex cursor-pointer items-center' onClick={()=>setShowNav(false)}>
+    <div className='cside hover:text-[#00e6e6] mt-7 border-b border-gray-400 w-[95%] '>
+    <Link href='/favourite' className=' pl-[6px] flex cursor-pointer items-center' onClick={()=>setShowNav(false)}>
     <AiFillHeart title='Favourites' size={25} color={'white'} className={` mb-7 `} />
     <p className=' font-semibold text-lg text-white mx-3 mb-7'>Favourites</p>
     </Link>
