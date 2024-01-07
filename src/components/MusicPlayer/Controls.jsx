7ec6ab7befcb24b64ec298068d3d23558e16fe88
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-01-08 02:51:24 +0400

    Update Controls.jsx

--- src/components/MusicPlayer/Controls.jsx
+++ src/components/MusicPlayer/Controls.jsx
@@ -10,7 +10,7 @@ import FavouriteButton from './FavouriteButton';
 const Controls = ({ isPlaying, repeat, setRepeat, shuffle, setShuffle, currentSongs, handlePlayPause, handlePrevSong, handleNextSong, activeSong, fullScreen, handleAddToFavourite, favouriteSongs, loading }) => {
   return (
     <div className="flex items-center justify-around md:w-80 text-lg lg:w-80 2xl:w-80 gap-4 sm:gap-0">
-      <FavouriteButton favouriteSongs={favouriteSongs} activeSong={activeSong} loading={loading} handleAddToFavourite={handleAddToFavourite} style={" md:block hidden"} />
+      <FavouriteButton favouriteSongs={favouriteSongs} activeSong={activeSong} loading={loading} handleAddToFavourite={handleAddToFavourite} style={" sm:block hidden"} />
       {
         !repeat ? (
           <TbRepeat title='Repeat' size={25} color={'white'} onClick={(e) => { e.stopPropagation(); setRepeat((prev) => !prev) }} className={`${!fullScreen ? 'hidden md:block' : ' m-3'} cursor-pointer`} />) : (
@@ -26,7 +26,7 @@ const Controls = ({ isPlaying, repeat, setRepeat, shuffle, setShuffle, currentSo
         <BsFillPlayFill size={45} color="#00e6e6" onClick={handlePlayPause} className="cursor-pointer" />
       )}
       {<MdSkipNext title='Next' size={35} color={currentSongs?.length ? '#ffff' : '#b3b3b3'} className="cursor-pointer" onClick={handleNextSong} />}
-      <TbArrowsShuffle title='Shuffle' size={25} color={shuffle ? '#00e6e6' : 'white'} onClick={(e) => { e.stopPropagation(); setShuffle((prev) => !prev) }} className={`${!fullScreen ? 'hidden md:block' : 'm-3'} cursor-pointer`} />
+      <TbArrowsShuffle title='Shuffle' size={25} color={shuffle ? '#00e6e6' : 'white'} onClick={(e) => { e.stopPropagation(); setShuffle((prev) => !prev) }} className={`${!fullScreen ? 'hidden sm:block' : 'm-3'} cursor-pointer`} />
       {activeSong?.downloadUrl?.[4]?.link &&
         <div className=' hidden md:block mt-1 '>
           <Downloader activeSong={activeSong} fullScreen={fullScreen} />
