cb4347743d32e7547f79818c9b2130d700e50b3e
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-01-07 23:21:16 +0400

    Update PlaylistModal.jsx

--- src/components/Sidebar/PlaylistModal.jsx
+++ src/components/Sidebar/PlaylistModal.jsx
@@ -51,7 +51,7 @@ const PlaylistModal = ({ show, setShow }) => {
     return (show &&
         <div>
             <div onClick={() => setShow(false)} className='fixed bg-black bg-opacity-50 top-0 left-0 w-full h-full z-50 flex justify-center items-center text-white'>
-                <div onClick={(e) => e.stopPropagation() } className='bg-[#020813a1] border-[1px] border-[#ffffff12] bg-opacity-80 backdrop-blur-[10px] rounded-lg w-[90%] sm:w-[auto]'>
+                <div onClick={(e) => e.stopPropagation() } className='bg-[#020813a1] border-[1px] border-[#ffffff1c] bg-opacity-80 backdrop-blur-[10px] rounded-lg w-[90%] sm:w-[auto]'>
                     <div className='flex justify-between items-center px-[20px] pb-[10px] pt-[20px]'>
                         <h1 className='text-lg font-semibold'>Create Playlist</h1>
                         <button onClick={() => setShow(false)} className='text-white text-lg font-semibold'>X</button>
