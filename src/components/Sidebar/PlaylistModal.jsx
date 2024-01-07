1e5f51289a977ab3315d02eae25bade80ba947f1
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-01-07 22:43:41 +0400

    Update PlaylistModal.jsx

--- src/components/Sidebar/PlaylistModal.jsx
+++ src/components/Sidebar/PlaylistModal.jsx
@@ -51,17 +51,17 @@ const PlaylistModal = ({ show, setShow }) => {
     return (show &&
         <div>
             <div onClick={() => setShow(false)} className='fixed bg-black bg-opacity-50 top-0 left-0 w-full h-full z-50 flex justify-center items-center text-white'>
-                <div onClick={(e) => e.stopPropagation() } className='bg-white/5 bg-opacity-80 backdrop-blur-sm rounded-lg w-[auto]'>
-                    <div className='flex justify-between items-center px-4 py-2'>
+                <div onClick={(e) => e.stopPropagation() } className='bg-[#020813a1] border-[1px] border-[#ffffff12] bg-opacity-80 backdrop-blur-[10px] rounded-lg w-[auto]'>
+                    <div className='flex justify-between items-center px-[20px] pb-[10px]'>
                         <h1 className='text-lg font-semibold'>Create Playlist</h1>
                         <button onClick={() => setShow(false)} className='text-white text-lg font-semibold'>X</button>
                     </div>
                     <div className='flex flex-col justify-center items-center'>
                         <div className=" flex gap-4 items-end mb-3">
                             <label className="hidden mr-9 lg:mr-11" htmlFor='name'>Name</label>
-                            <input onFocus={handleFocus} onBlur={handleBlur} onChange={handleChange} value={name} name='name' type="text" placeholder="Playlist Name" required className='mx-[20px] appearance-none bg-[#f5deb300] bg-opacity-100 backdrop-blur-sm border-b border-white focus:outline-none text-base lg:text-lg' />
+                            <input onFocus={handleFocus} onBlur={handleBlur} onChange={handleChange} value={name} name='name' type="text" placeholder="Playlist Name" required className='my-[5px] py-[10px] pl-[15px] border-[#ffffff12] border-[1px] rounded-[10px] bg-[#020813a1] mx-[20px] appearance-none bg-[#f5deb300] bg-opacity-100 backdrop-blur-sm border-b border-white focus:outline-none text-base lg:text-lg' />
                         </div>
-                            <button onClick={handelCreate} className='text-sm group font-semibold mb-7 flex gap-2 border-[1.5px] border-white rounded-lg px-2 items-center py-2'>
+                            <button onClick={handelCreate} className='group-hover:text-[#00e6e6] text-sm group font-semibold mb-7 flex gap-2 border-[1.5px] border-white rounded-lg px-[15px] items-center py-[10px] border-[1px] border-[#ffffff12]'>
                                 {
                                     loading ? <div className='custom-loader w-[20px] h-[20px]'></div> : <FaPlus className=' w-[20] h-[20px] group-hover:text-[#00e6e6]' />
                                 }
