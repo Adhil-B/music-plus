81fe7665eb2e6c48a68d2494cd9136c201c73520
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-01-08 01:48:29 +0400

    Update Track.jsx

--- src/components/MusicPlayer/Track.jsx
+++ src/components/MusicPlayer/Track.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Track = ({ isPlaying, isActive, activeSong, fullScreen }) => (
   <div className={`flex-1 flex items-center justify-start ${fullScreen ? 'hidden':''}`}>
-    <div className={`${isPlaying && isActive ? 'animate-[spin_15s_linear_infinite]' : ''} hidden sm:block h-16 w-16 mr-4`}>
+    <div className={`${isPlaying && isActive ? 'animate-[spin_15s_linear_infinite]' : ''} hidden sm:block h-16 w-16`}>
       <img src={activeSong?.image?.[2].link || 'https://musicplus-web.vercel.app/Music.png'} alt="cover art" className="h-[100%] object-cover rounded-full"/>
     </div>
-    <div className={`w-[190px] select-none cursor-pointer`}>
+    <div className={`w-[190px] ml-4 select-none cursor-pointer`}>
       <p className="truncate text-white font-bold text-lg">
         {activeSong?.name ? activeSong?.name.replace("&#039;", "'").replace("&amp;", "&").replaceAll('&quot;','"') : 'Song'}
       </p>
