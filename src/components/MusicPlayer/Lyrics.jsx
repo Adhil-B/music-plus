cd909f004d807120f16893f43814471f0aa39080
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-01-20 06:17:22 +0400

    Update Lyrics.jsx

--- src/components/MusicPlayer/Lyrics.jsx
+++ src/components/MusicPlayer/Lyrics.jsx
@@ -60,7 +60,7 @@ const Lyrics = ({ activeSong, scrollableDivRef }) => {
 
     return (
         <div onClick={(e) => { e.stopPropagation(); }} >
-            <div className='flex justify-center items-center w-full mt-[-8.5vh] sm:mt-[15px]'>
+            <div className='flex justify-center items-center w-full mt-[-9.5vh] mb-[3vh] sm:mt-[15px] sm:mb-auto'>
                 <button onClick={() => { setNum(num+1); setActiveTab('queue') }} className={`${activeTab === 'queue' ? 'border-[#00e6e6] border-b-2' : ''} text-white text-xl m-3 sm:mt-0 font-medium `}>Queue</button>
                 <button onClick={() => { setNum(num+1); setActiveTab('lyrics') }} className={`${activeTab === 'lyrics' ? 'border-[#00e6e6] border-b-2' : ''} text-white text-xl m-3 sm:mt-0  font-medium`}>Lyrics</button>
             </div>
