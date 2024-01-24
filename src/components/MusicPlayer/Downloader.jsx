3e8628f446af0d1830771ce676bbe8948416384c
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-01-24 15:46:56 +0400

    Update Downloader.jsx

--- src/components/MusicPlayer/Downloader.jsx
+++ src/components/MusicPlayer/Downloader.jsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { MdOutlineFileDownload } from 'react-icons/md';
 import useDownloader from 'react-use-downloader';
 import {MdDownloadForOffline, MdFileDownloadDone} from 'react-icons/md'
