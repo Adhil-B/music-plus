4ba55a11a37e493bf175701d8e76762d2f85e104
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-01-26 13:04:11 +0400

    Update Downloader.jsx

--- src/components/MusicPlayer/Downloader.jsx
+++ src/components/MusicPlayer/Downloader.jsx
@@ -10,17 +10,22 @@ const Downloader = ({activeSong, icon}) => {
     const imageUrl = activeSong?.image?.[2]?.link;
     const filename = `${activeSong?.name?.replace("&#039;","'")?.replace("&amp;","&")?.replaceAll('&quot;','"')}.mp3`
     const artists = activeSong?.primaryArtists;
-    const [done, setDone] = useState(false);
+    const [done, setDone] = useState([false,false,0]);
 
-  useEffect(() => {
+useEffect(() => {	
+setDone([percentage == 100,isInProgress,percentage])
+}, [percentage, isInProgress,]);	
+	
+useEffect(() => {
 try{
 browserFileStorage.init('downloads').then((status) => {
 if(status.initial) {}
 }).catch((error) => {});
 }catch(err) {}
-setDone(localStorage.getItem("downloaded") ? localStorage.getItem("downloaded").includes(filename) : false);
+setDone([false,done[1],done[2]]);
+}, []);
 
-  }, []);
+
     
   return (
     <div onClick={(e)=>{e.stopPropagation();
@@ -70,24 +75,25 @@ setDone(localStorage.getItem("downloaded") ? localStorage.getItem("downloaded").
     // Every time progress occurs
     const percent = parseInt((e.loaded / e.total)*100);
 
-    var node = document.getElementById("xhr1");
-    var div = document.createElement('div');
-    var div1 = document.createElement('div1');
-    div.id="xhr2";
-    div.classList.add("text-white","font-extrabold","text-xs","m-");
-    div.innerHTML=percent;
-    div1.innerHTML = `<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" color="" style="color:rgb(0, 230, 230)" height="25" width="25" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M18 15v3H6v-3H4v3c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-3h-2zm-1-4l-1.41-1.41L13 12.17V4h-2v8.17L8.41 9.59 7 11l5 5 5-5z"></path></svg>`;
-    while( node.firstChild )
-    node.removeChild( node.firstChild );
+    //var node = document.getElementById("xhr1");
+    //var div = document.createElement('div');
+    //var div1 = document.createElement('div1');
+    //div.id="xhr2";
+    //div.classList.add("text-white","font-extrabold","text-xs","m-");
+    //div.innerHTML=percent;
+    //div1.innerHTML = `<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" color="" style="color:rgb(0, 230, 230)" height="25" width="25" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M18 15v3H6v-3H4v3c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-3h-2zm-1-4l-1.41-1.41L13 12.17V4h-2v8.17L8.41 9.59 7 11l5 5 5-5z"></path></svg>`;
+    //while( node.firstChild )
+    //node.removeChild( node.firstChild );
     if (percent != 100){
-    node.appendChild( div ); 
+    setDone([false,true,percent]);
+    //node.appendChild( div ); 
 
     }else{
 	    
 	    
-    document.getElementById("xhr1").classList.remove('download-button','flex', 'justify-center', 'items-center');
-    node.appendChild( div1 ); 
-    setDone(true)
+    //document.getElementById("xhr1").classList.remove('download-button','flex', 'justify-center', 'items-center');
+    //node.appendChild( div1 ); 
+    setDone([true,false,100]);
 	    
     }    }       
                         
@@ -106,12 +112,12 @@ setDone(localStorage.getItem("downloaded") ? localStorage.getItem("downloaded").
     }} className={`flex  mb-1 cursor-pointer w-7`}>
         
         
-    <div title={isInProgress ?'Downloading' : 'Download'} id="xhr1" className={isInProgress ? 'download-button flex justify-center items-center':''}>
+    <div title={done[1] ?'Downloading' : 'Download'} id="xhr1" className={done[1] ? 'download-button flex justify-center items-center':''}>
         {
-            isInProgress ? 
-            <div id="xhr2" className=' text-white font-extrabold text-xs m-'>{percentage}</div>
+            done[1] ? 
+            <div id="xhr2" className=' text-white font-extrabold text-xs m-'>{done[2]}</div>
             :
-              done ? <MdFileDownloadDone size={25} color={'#00e6e6'}/> : <MdOutlineFileDownload  size={25} color={'#ffff'}/>
+              done[0] ? <MdFileDownloadDone size={25} color={'#00e6e6'}/> : <MdOutlineFileDownload  size={25} color={'#ffff'}/>
         }
       </div>
     </div>
