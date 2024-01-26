40d7292ac7a941f882cfa605f9a9d392b1dae432
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-01-27 01:18:20 +0400

    Update SongsList.jsx

--- src/components/SongsList.jsx
+++ src/components/SongsList.jsx
@@ -110,7 +110,7 @@ setAllfilenames(localStorage?.getItem("downloaded") ? localStorage.getItem("down
                     }
 
                   </div>
-                  <div className=" w-[40vw] md:w-64">
+                  <div className=" w-[40vw] md:w-64 mb-3">
                     <div className="flex gap-[0.2rem] items-center">
                     <MdDownloadForOffline size={16} className={allfilenames.includes(`img-${song?.name?.replace("&#039;","'")?.replace("&amp;","&")?.replaceAll('&quot;','"')}`) && allfilenames.includes(`${song?.name?.replace("&#039;","'")?.replace("&amp;","&")?.replaceAll('&quot;','"')}.mp3`) ? 'min-w-[17px]' : 'hidden'}/>
                     <p className="text-sm lg:text-lg font-semibold truncate">{
