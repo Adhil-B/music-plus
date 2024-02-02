1d93bdbb492931dbee78723be82c57e890435b02
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-02-02 18:10:18 +0400

    Update script-1.js

--- public/script-1.js
+++ public/script-1.js
@@ -1,3 +1,21 @@
       if (typeof navigator.serviceWorker !== 'undefined') {
           navigator.serviceWorker.register('sw.js')
       }
+
+browserFileStorage.init('downloads').then((status) => {
+if(status.initial) {
+	setPdd(true);
+}
+browserFileStorage.list().then((filenames) => {
+localStorage?.setItem("downloaded" , filenames);
+}).catch((error) => {})
+	
+}).catch((error) => {
+	if(error.alreadyInit) {
+	browserFileStorage.list().then((filenames) => {
+        //setAllfilenames(filenames)
+	localStorage?.setItem("downloaded" , filenames)
+        }).catch((error) => {})
+		
+	}
+});
