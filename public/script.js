703c17efe97fa9dc04fafd399ea42c8c51353aad
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-02-10 03:38:08 +0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -460,7 +460,7 @@ let observer = new MutationObserver((mutations) => {
     if (oldValue !== newValue) {
      // if window.location.pathname != ""{
         change()
-	if (mutation.type === "attributes" && document.querySelector('[aria-label="Share this video"].yt-spec-button-shape-next--overlay.yt-spec-button-shape-next--tonal:active') != null && mutation.target == document.body){
+	if (mutation.type === "attributes" && document.querySelector('[aria-label="Share this video"].yt-spec-button-shape-next--overlay.yt-spec-button-shape-next--tonal:active') != null ){
 
 	const urlParams1 = window.location.pathname;
   	console.log("share:https://youtube.com"+urlParams1);
