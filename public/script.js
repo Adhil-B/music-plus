53dbc922bbfe2cdfbae634935d8d8ac10db9cf10
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-02-10 04:18:25 +0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -460,14 +460,12 @@ let observer = new MutationObserver((mutations) => {
     if (oldValue !== newValue) {
      // if window.location.pathname != ""{
         change()
-	if (mutation.type === "attributes" && document.querySelector('[aria-label="Share this video"].yt-spec-button-shape-next--overlay.yt-spec-button-shape-next--tonal:active') != null && mutation.share ? mutation.share : true){
+	if (mutation.type === "attributes" && document.querySelector('[aria-label="Share this video"].yt-spec-button-shape-next--overlay.yt-spec-button-shape-next--tonal:active') != null && document.body.matches('[bottom-sheet-open=""]')){
 
 	const urlParams1 = window.location.pathname;
   	console.log("share:https://youtube.com"+urlParams1);
-        mutation.share = false;
+	document.body.removeAttribute('bottom-sheet-open')
   	
-	}else{
-	mutation.share = true;
 	}
         
       
