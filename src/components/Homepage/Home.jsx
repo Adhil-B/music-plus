606fc05093a9e2689f86b04a8e8a0c5eadafa519
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-02-23 16:29:39 +0400

    Update Home.jsx

--- src/components/Homepage/Home.jsx
+++ src/components/Homepage/Home.jsx
@@ -18,11 +18,7 @@ import ytdl from "react-native-ytdl"
 
 
 const Home = () => {
-  async function myFunction() {
-  const youtubeURL = 'http://www.youtube.com/watch?v=04GiqLjRO3A';
-const urls = await ytdl(youtubeURL, { quality: 'highestaudio' });
-    return urls;
-  }
+
   const [data, setData] = useState("");
   const [data2, setData2] = useState("");
   const [data3, setData3] = useState("");
@@ -68,7 +64,7 @@ const urls = await ytdl(youtubeURL, { quality: 'highestaudio' });
       setData3(res3);
       setSongR(res3 ? res3["recommendations"] : []);        
       setLoading2(false);
-      console.log(await myFunction())
+
     };
 
     fetchDataa();
