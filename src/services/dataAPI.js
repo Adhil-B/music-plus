48490ae0ddf05583f57c88429777b1e37c0595c7
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-03-06 16:39:17 +0400

    Update dataAPI.js

--- src/services/dataAPI.js
+++ src/services/dataAPI.js
@@ -434,14 +434,17 @@ export async function getSearchedData(query) {
     const response = await fetch(`https://saavn.dev/search/all?query=${query}`);
     const data = await response.json();
     
-    const response1 = await fetch(`https://ytpi.vercel.app/search?query=${query}&filter=songs`);
+    const response1 = await fetch(`https://ytpi.vercel.app/search?query=${query}`);
     const data1 = await response1.json();
     const data2 = [];
     if (query.includes("youtube") || data.data["songs"]["results"].length < 3){
     for (let x of data1) {
+      if !("Top result Songs".include(x['category']) && x['videoId'] != null) { continue; }
       let art = [];
       for (let arti of x["artists"]) {
+        if (arti["id"] != null) {
         art.push(arti["name"])
+        }
       }
       x["primaryArtists"] = art.reverse().join();
       x["duration"] = x["lengthSeconds"];
