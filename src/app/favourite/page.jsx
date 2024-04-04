4c52e5662760a14f394edbc8d2e0fdf718fea4fa
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-04-05 01:46:13 +0400

    Update page.jsx

--- src/app/favourite/page.jsx
+++ src/app/favourite/page.jsx
@@ -17,7 +17,6 @@ const page = () => {
   const [start, setStart] = useState(false);
   const { status } = useSession();
   const { pdownloading } = useSelector((state) => state.player);
-  const { pdownloading2 } = useSelector((state) => state.player);
 
 useEffect(() => {
 try{
@@ -113,7 +112,7 @@ for (let i in favouriteSongs) {
 	    let pending = localStorage?.getItem("downloading") ? JSON.parse(localStorage?.getItem("downloading"))?.filter((song) => song?.filename !== filename) : [];
 	    let newlist = [...pending, {filename: filename, artist: artists, duration: duration, songUrl: songUrl}];
 	    localStorage?.setItem("downloading" , JSON.stringify(newlist));
-	    dispatch(setPdownloading2(newlist));
+	    dispatch(setPdownloading(newlist));
 	    browserFileStorage.list().then((filenames) => {
 	    localStorage?.setItem("downloaded" , filenames);
             setDownloading([...downloading, song.id])
