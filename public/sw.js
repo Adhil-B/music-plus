3179e3ef7424cab1ef6064284fc2cd723fe69abe
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-04-16 19:23:42 +0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -24,18 +24,6 @@ if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
 
-// remove old cache if any
-self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-    const cacheNames = await caches.keys();
-
-    await Promise.all(cacheNames.map(async (cacheName) => {
-      if (self.cacheName !== cacheName) {
-        await caches.delete(cacheName);
-      }
-    }));
-  })());
-});
 
 self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
