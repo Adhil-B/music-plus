283ab31bbc5651bb5d0003cb3cd6b38938b5e536
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-04-17 15:02:35 +0400

    Update script-1.js

--- public/script-1.js
+++ public/script-1.js
@@ -1,3 +1,41 @@
       if (typeof navigator.serviceWorker !== 'undefined') {
           navigator.serviceWorker.register('sw.js')
       }
+
+// main.js
+
+let installPrompt = null;
+const installButton = document.querySelector(".install");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installPrompt = event;
+  installButton.removeAttribute("hidden");
+});
+
+// main.js
+
+installButton.addEventListener("click", async () => {
+  if (!installPrompt) {
+    return;
+  }
+  const result = await installPrompt.prompt();
+  console.log(`Install prompt was: ${result.outcome}`);
+  disableInAppInstallPrompt();
+});
+
+function disableInAppInstallPrompt() {
+  installPrompt = null;
+  installButton.setAttribute("hidden", "");
+}
+
+// main.js
+
+window.addEventListener("appinstalled", () => {
+  disableInAppInstallPrompt();
+});
+
+function disableInAppInstallPrompt() {
+  installPrompt = null;
+  installButton.setAttribute("hidden", "");
+}
