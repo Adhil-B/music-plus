683b78829d9a6bc76ca7aefc5b1cc6c7858f174c
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-04-20 01:13:25 +0400

    Update dataAPI.js

--- src/services/dataAPI.js
+++ src/services/dataAPI.js
@@ -192,9 +192,9 @@ function transformList(list) {
     sresponse =  await fetch(`https://saavn.dev/api/search/songs?query=${x[0]["snippet"]["title"]+' '+x[0]["snippet"]["channelTitle"].replace(' - Topic', '')}`);
     sdata22 = await sresponse.json();
     sdata99 = sdata22.data?.results?.slice(0,2)
-    x[0]['min'] = x["contentDetails"]["duration"].includes('M') ? parseInt(x["contentDetails"]["duration"].split("PT")[1].split("M")[0])*60 : parseInt('0');
-    x[0]['sec'] = x["contentDetails"]["duration"].includes('M') ? parseInt(x["contentDetails"]["duration"].split("M")[1].split("S")[0]) : parseInt(x["contentDetails"]["duration"].split("PT")[1].split("S")[0]);
-    x[0]["duration"] = x['min'] + x['sec'];
+    x[0]['min'] = x[0]["contentDetails"]["duration"].includes('M') ? parseInt(x[0]["contentDetails"]["duration"].split("PT")[1].split("M")[0])*60 : parseInt('0');
+    x[0]['sec'] = x[0]["contentDetails"]["duration"].includes('M') ? parseInt(x[0]["contentDetails"]["duration"].split("M")[1].split("S")[0]) : parseInt(x[0]["contentDetails"]["duration"].split("PT")[1].split("S")[0]);
+    x[0]["duration"] = x[0]['min'] + x[0]['sec'];
     topsong = Object.values(sdata99).filter(entry => (Math.abs(parseInt(entry["duration"]) - (x[0]["duration"])) < 8) && (entry["name"].replace("(", "").replace(")", "").includes(x[0]["snippet"]["title"].split(' (')[0])) );
     
     /*
