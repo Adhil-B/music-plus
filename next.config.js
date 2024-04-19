9db53379d0441fffdb11b7f022be9bf6f41aa04a
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-04-19 20:14:03 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['c.saavncdn.com','static.saavncdn.com','www.jiosaavn.com','musicplus-web.vercel.app','i.ytimg.com'],
+        domains: ['lh3.googleusercontent.com','c.saavncdn.com','static.saavncdn.com','www.jiosaavn.com','musicplus-web.vercel.app','i.ytimg.com'],
     },
 }
 
