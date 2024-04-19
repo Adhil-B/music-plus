01ed326aa7cac2db7930314a0162cb9e968cc54e
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-04-19 18:00:56 +0400

    Update dataAPI.js

--- src/services/dataAPI.js
+++ src/services/dataAPI.js
@@ -162,7 +162,7 @@ function transformList(list) {
     sresponse =  await fetch(`https://saavn.dev/api/search/songs?query=${x[0]["snippet"]["title"]+' '+x[0]["snippet"]["channelTitle"].replace(' - Topic', '')}`);
     sdata22 = await sresponse.json();
     sdata99 = sdata22.data?.results?.slice(0,2)
-    topsong = Object.values(sdata99).filter(entry => (Math.abs(parseInt(entry["duration"]) - (parseInt(x[0]["contentDetails"]["duration"].split("PT")[1].split("M")[0])*60 + parseInt(x[0]["contentDetails"]["duration"].split("M")[1].split("S")[0])) < 8) && (entry["name"].replace("(", "").replace(")", "").includes(x[0]["snippet"]["title"].split(' (')[0])));
+    topsong = Object.values(sdata99).filter(entry => (Math.abs(parseInt(entry["duration"]) - (parseInt(x[0]["contentDetails"]["duration"].split("PT")[1].split("M")[0])*60 + parseInt(x[0]["contentDetails"]["duration"].split("M")[1].split("S")[0])) < 8) && (entry["name"].replace("(", "").replace(")", "").includes(x[0]["snippet"]["title"].split(' (')[0]))));
     
     /*
     sresponse =  await fetch(`https://saavn.dev/api/search/songs?query=${x["title"].split(' (From')[0]}&page=1&limit=2`);
