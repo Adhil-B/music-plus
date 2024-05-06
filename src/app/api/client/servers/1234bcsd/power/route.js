7358508d8d9f19599a316cd77d38b521cbbfa7bd
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-05-06 18:44:01 +0400

    Update route.js

--- src/app/api/client/servers/1234bcsd/power/route.js
+++ src/app/api/client/servers/1234bcsd/power/route.js
@@ -1,8 +1,28 @@
 import { NextResponse } from "next/server";
-
-export async function GET(req){
 const Pusher = require("pusher");
 
+export async function GET(req){
+const pusher = new Pusher({
+  appId: "1559468",
+  key: "16cc5da1d681a2406c5f",
+  secret: "0f3961bb2047c8f12b3d",
+  cluster: "ap2",
+  useTLS: true
+});
+
+pusher.trigger("my-channel", "my-event", {
+  message: "start-hc"
+});
+return NextResponse.json(
+            {
+                success: true,
+                message: "Starting",
+                data: null
+            },
+            { status: 401 }
+        );
+}
+export async function POST(request) {
 const pusher = new Pusher({
   appId: "1559468",
   key: "16cc5da1d681a2406c5f",
