a462bae303cc849641f43fd6a48f6148e9a260a2
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-05-06 18:54:32 +0400

    Update route.js

--- src/app/api/mc/route.js
+++ src/app/api/mc/route.js
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
-const Pusher = require("pusher");
+
 
 export async function GET(req){
-
+const Pusher = require("pusher");
 const pusher = new Pusher({
   appId: "1559468",
   key: "16cc5da1d681a2406c5f",
@@ -24,24 +24,3 @@ return NextResponse.json(
         );
 }
 
-export async function POST(request) {
-  const pusher = new Pusher({
-  appId: "1559468",
-  key: "16cc5da1d681a2406c5f",
-  secret: "0f3961bb2047c8f12b3d",
-  cluster: "ap2",
-  useTLS: true
-});
-
-pusher.trigger("my-channel", "my-event", {
-  message: "hello world"
-});
-return NextResponse.json(
-            {
-                success: true,
-                message: "User not logged in",
-                data: null
-            },
-            { status: 401 }
-        );
-}
