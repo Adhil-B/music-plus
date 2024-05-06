a6442dec57c9ad0f30f9462559f8c98dcb019fa8
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-05-06 23:47:41 +0400

    Update route.js

--- src/app/api/client/servers/1234bcsd/power/route.js
+++ src/app/api/client/servers/1234bcsd/power/route.js
@@ -2,26 +2,7 @@ import { NextResponse } from "next/server";
 const Pusher = require("pusher");
 
 export async function POST(req) {
-
-const pusher = new Pusher({
-  appId: "1559468",
-  key: "16cc5da1d681a2406c5f",
-  secret: "0f3961bb2047c8f12b3d",
-  cluster: "ap2",
-  useTLS: false
-});
-
-pusher.trigger("my-channel", "my-event", {
-  message: "start-hc"
-});
-return NextResponse.json(
-            {
-                success: true,
-                message: "Starting",
-                data: null
-            },
-            { status: 200 }
-        );
+const favresponse = await fetch("https://flask-hello-world-theta-dusky.vercel.app/api/client/servers/1234bcsd/power");
 }
 export async function GET(reqq) {
 
