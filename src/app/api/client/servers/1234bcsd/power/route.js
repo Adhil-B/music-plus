a1903eef274f5c57065cf897275228bea30fa48a
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-05-06 19:18:27 +0400

    Update route.js

--- src/app/api/client/servers/1234bcsd/power/route.js
+++ src/app/api/client/servers/1234bcsd/power/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
+const response = await fetch(`https://musicplus.ddns.net/api/mc`);
 const Pusher = require("pusher");
 
 const pusher = new Pusher({
@@ -12,31 +13,7 @@ const pusher = new Pusher({
 });
 
 pusher.trigger("my-channel", "my-event", {
-  message: "hello world"
-});
-return NextResponse.json(
-            {
-                success: true,
-                message: "Starting",
-                data: null
-            },
-            { status: 200 }
-        );
-}
-
-export async function GET(reqq) {
-const Pusher = require("pusher");
-
-const pusher = new Pusher({
-  appId: "1559468",
-  key: "16cc5da1d681a2406c5f",
-  secret: "0f3961bb2047c8f12b3d",
-  cluster: "ap2",
-  useTLS: true
-});
-
-pusher.trigger("my-channel", "my-event", {
-  message: "hello world"
+  message: "start-hc"
 });
 return NextResponse.json(
             {
