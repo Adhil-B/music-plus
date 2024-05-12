6a5293506b024863f3358d183dc330bb240ded12
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-05-12 23:08:12 +0400

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,14 +10,14 @@ import { Toaster } from "react-hot-toast";
 import AuthProvider from "./AuthProvider";
 import { Poppins } from "next/font/google";
 import Script from "next/script";
-
+import { useSelector } from 'react-redux';
 
 const poppins = Poppins({
   weight: "500",
   subsets: ["latin-ext"],
   display: "swap",
 });
-
+const { fullScreen } = useSelector((state) => state.player);
 export const metadata = {
   title: "Music+",
   description: "Music streaming app",
@@ -31,6 +31,7 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
 
@@ -48,7 +49,7 @@ export default function RootLayout({ children }) {
             <Toaster />
             {children}
             <div className="h-20"></div>
-            <div className="fixed  bottom-0 left-0 right-0 flex backdrop-blur-[100px] rounded-t-3 z-50">
+            <div className=`fixed ${fullScreen ? '' : 'sm:bottom-[1vh] sm:left-[3vw] sm:right-[3vw] sm:rounded-[10px] sm:shadow-[0px_10px_10px_10px_#000000ab]'} bottom-0 left-0 right-0 flex backdrop-blur-[100px] rounded-t-3 z-50`>
               <MusicPlayer />
 
             </div>
