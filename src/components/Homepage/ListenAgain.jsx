4d169fc33c6bdd9198b77b5575a6f32ca03659fa
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-05-12 16:13:13 +0400

    Update ListenAgain.jsx

--- src/components/Homepage/ListenAgain.jsx
+++ src/components/Homepage/ListenAgain.jsx
@@ -24,7 +24,7 @@ const ListenAgain = () => {
         songHistory?.length > 0 && (
           <div>
             <h2 className=" text-white mt-4 text-2xl lg:text-3xl font-semibold mb-4 ">Listen Again</h2>
-            <div className="overflow-y-scroll hideScrollBar max-h-[170px] bg-[hsla(0,0%,100%,.05)] pt-[3px] pr-[15px] pb-[15px] pl-[20px] rounded-lg grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-10">
+            <div className="overflow-y-scroll hideScrollBar max-h-[170px] sm:max-h-[161px] bg-[hsla(0,0%,100%,.05)] pt-[3px] pr-[15px] pb-[15px] pl-[20px] rounded-lg grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-10">
               {
                 songHistory?.map((song, index) => (
                   <ListenAgainCard key={song?.id} song={song} SongData={songHistory} index={index} />
