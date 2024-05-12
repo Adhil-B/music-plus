63dec86e666246c57e9c436fea240f47a105f297
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-05-12 16:10:11 +0400

    Update ListenAgainCard.jsx

--- src/components/ListenAgainCard.jsx
+++ src/components/ListenAgainCard.jsx
@@ -39,7 +39,7 @@ const ListenAgainCard = ({song, index, SongData}) => {
                
               </div>
               <div className="w-[60vw] sm:w-24 md:w-64">
-                <p className="text-sm lg:text-lg font-semibold truncate">{
+                <p className="text-sm lg:text-[1rem] font-semibold truncate">{
                     song?.name?.replace("&#039;", "'")?.replace("&amp;", "&")?.replaceAll('&quot;','"')
                 }</p>
                 <p className="text-gray-400 truncate text-xs">{song?.primaryArtists}</p>
