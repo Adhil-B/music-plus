43fc21efdf34e2ec2bd242902febeb378e0dc2cf
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-05-12 20:42:58 +0400

    Update route.js

--- src/app/api/audio/route.js
+++ src/app/api/audio/route.js
@@ -1,4 +1,10 @@
+import { useRouter } from "next/router";
 
 export async function GET(req){
+  const router = useRouter();
+  const { query } = router;
 
+  const parameter1 = query.videoId;
+  return parameter1;
+  
 }
