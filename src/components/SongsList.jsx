e18eef742f33c05499a232bbd50713f12dbe2872
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-05-12 22:34:57 +0400

    Update SongsList.jsx

--- src/components/SongsList.jsx
+++ src/components/SongsList.jsx
@@ -89,7 +89,7 @@ useEffect(() => {
 
   return (
     <>
-      <div className="aside bg-[#02081363] pt-[3px] pr-[15px] pb-[15px] pl-[20px] rounded-lg">
+      <div className="aside bg-[#02081363] pt-[3px] pr-[15px] pb-[8px] pl-[20px] rounded-lg">
         {
           !loading && SongData?.length > 0 ? (
             SongData?.map((song, index) => (
