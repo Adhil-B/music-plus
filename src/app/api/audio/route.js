39605914f13d6bbe1d44cc10d11ed00f66a0a24b
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-05-12 20:49:56 +0400

    Update route.js

--- src/app/api/audio/route.js
+++ src/app/api/audio/route.js
@@ -1,8 +1,6 @@
-import { useSearchParams } from "next/navigation";
+
 
 export async function GET(req){
-  const searchParams = useSearchParams();
-  let property1 = searchParams.get("id");
-  return property1;
+
   
 }
