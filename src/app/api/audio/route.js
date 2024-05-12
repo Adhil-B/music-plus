8b8d6ce15c600bed32f5b2e959e2b402cc7efdd2
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-05-12 20:37:36 +0400

    Update route.js

--- src/app/api/audio/route.js
+++ src/app/api/audio/route.js
@@ -1,5 +1,5 @@
 'use client'
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next-navigation'
 export async function GET(req){
   const searchParams = useSearchParams()
  
