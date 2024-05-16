27631bc58fd0fdc4171787d4c80cb203a15643e1
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-05-16 15:46:07 +0400

    Update route.js

--- src/app/api/audio/route.js
+++ src/app/api/audio/route.js
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { redirect } from 'next/navigation'
 import { getAudio } from "@/services/dataAPI";
-export async function GET(req){
-  redirect(await getAudio(req.nextUrl.searchParams.get("audioId"))); 
+export async function GET(request: NextRequest){
+  redirect(await getAudio(request.nextUrl.searchParams.get("audioId"))); 
 }
