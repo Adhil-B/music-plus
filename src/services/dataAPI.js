f11e3e8257e4e5d4f5aa8573273e21d0f2194764
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-05-16 15:55:38 +0400

    Update dataAPI.js

--- src/services/dataAPI.js
+++ src/services/dataAPI.js
@@ -322,7 +322,6 @@ export async function getAudio(id) {
     let sdata99;
     let topsong = [];
   
-    if (id2.length < 2 && id.split(',').length < 2) {
     sresponse =  await fetch(`https://saavn.dev/api/search/songs?query=${x[0]["snippet"]["title"]+' '+x[0]["snippet"]["channelTitle"].replace(' - Topic', '')}`);
     sdata22 = await sresponse.json();
     sdata99 = sdata22.data?.results?.slice(0,2)
@@ -330,7 +329,7 @@ export async function getAudio(id) {
     x[0]['sec'] = x[0]["contentDetails"]["duration"].includes('M') ? parseInt(x[0]["contentDetails"]["duration"].split("M")[1].split("S")[0]) : parseInt(x[0]["contentDetails"]["duration"].split("PT")[1].split("S")[0]);
     x[0]["duration"] = x[0]['min'] + x[0]['sec'];
     topsong = Object.values(sdata99).filter(entry => (Math.abs(parseInt(entry["duration"]) - (x[0]["duration"])) < 8) && (entry["name"].replace("(", "").replace(")", "").includes(x[0]["snippet"]["title"].split(' (')[0])) );    
-    }
+    
     if (topsong.length > 0){
     const response9 = await fetch(`https://jiosaavn-api-gilt.vercel.app/songs?id=${topsong[0]['id']}`);
     const data9 = await response9.json();
