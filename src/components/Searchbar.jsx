bd04956b4451da0e61f3667a4a77a23dfed0b930
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-12-08 14:05:05 +0530

    Update Searchbar.jsx

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -62,7 +62,25 @@ const Searchbar = () => {
 
         
       </div>
+      <div className={`${isTyping ? '':'hidden'}`}>
+         <div
+            onClick={() => {
+                handlePlayClick(song,index);
+            }}
+             className={`flex w-40 md:w-80 items-center mt-5 cursor-pointer group border-b-[2px] border-[#ffffff00] justify-between ${activeSong?.id === song?.id && " text-[#00e6e6]"}`}>
+                <div className="flex items-center gap-5">
+
+              <div className="w-[60vw] sm:w-24 md:w-64">
+                <p className="text-sm lg:text-[1rem] font-semibold truncate">{
+                    hello
+                }</p>
+                <p className="text-gray-400 truncate text-xs">cool</p>
+              </div>
+              </div>
+            </div>
+    </div>
     </form>
+    
   );
 };
 
