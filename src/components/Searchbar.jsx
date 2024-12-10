2c4b010ad0d4154b2df4d53e47241eb13b4f0c62
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-12-10 05:50:19 +0530

    Update Searchbar.jsx

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -87,7 +87,10 @@ const Searchbar = () => {
 
         
       </div>
-      <div className={`${isTyping ? '':'hidden'} ${suggestion.length < 1 ? 'hidden':''} !pl-[0px] !mt-[5px] !rounded-[30px] !h-auto z-[39] fixed t-25vh asearch w-[87%] p-[10px] `}>
+
+   
+    </form>
+            <div className={`${isTyping ? '':'hidden'} ${suggestion.length < 1 ? 'hidden':''} !pl-[0px] !mt-[5px] !rounded-[30px] !h-auto z-[39] fixed t-50vh asearch w-[87%] p-[10px] `}>
          <div             className={`flex w-40 md:w-80 items-center mt-[10px] cursor-pointer group border-b-[2px] border-[#ffffff00] justify-between`}>
                 <div className="grid items-center gap-5">
 
@@ -108,8 +111,6 @@ const Searchbar = () => {
               </div>
             </div>
     </div>
-   
-    </form>
     </div>
   );
 };
