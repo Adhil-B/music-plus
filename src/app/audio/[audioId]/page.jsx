aa171286e987786ce976b77524238349bfa5388e
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-12-15 21:44:39 +0530

    Update page.jsx

--- src/app/audio/[audioId]/page.jsx
+++ src/app/audio/[audioId]/page.jsx
@@ -3,10 +3,10 @@ import { Innertube, UniversalCache } from 'youtubei.js';
 const page = async({ params }) => {
 const yt = await Innertube.create({ cache: new UniversalCache(true) });
 
-async function getVideoInfo(videoId: string) {
+async function getVideoInfo() {
   const videoInfo = await yt.actions.execute('/player', {
     // You can add any additional payloads here, and they'll merge with the default payload sent to InnerTube.
-    videoId,
+    'HPnLom-vBfc',
     client: 'YTMUSIC', // InnerTube client to use.
     parse: true // tells YouTube.js to parse the response (not sent to InnerTube).
   });
