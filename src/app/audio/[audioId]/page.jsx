606f6a8825af06613a30efa757fb59b893089502
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-12-15 21:37:52 +0530

    Update page.jsx

--- src/app/audio/[audioId]/page.jsx
+++ src/app/audio/[audioId]/page.jsx
@@ -1,13 +1,24 @@
-import { redirect } from 'next/navigation'
-import { getAudio } from "@/services/dataAPI";
-const page = ({ params }) => {
+import { Innertube, UniversalCache } from 'youtubei.js';
 
-useEffect(() => {
-    const fetchDataa = async () => {
-    redirect(await getAudio(params.audioId)); 
-};
-    fetchDataa();   
-}, []);
+const page = ({ params }) => {
+const yt = await Innertube.create({ cache: new UniversalCache(true) });
+
+async function getVideoInfo(videoId: string) {
+  const videoInfo = await yt.actions.execute('/player', {
+    // You can add any additional payloads here, and they'll merge with the default payload sent to InnerTube.
+    videoId,
+    client: 'YTMUSIC', // InnerTube client to use.
+    parse: true // tells YouTube.js to parse the response (not sent to InnerTube).
+  });
+
+  return videoInfo;
+}
+
+const videoInfo = await getVideoInfo('jLTOuvBTLxA');
+return(console.info(videoInfo))
+
+
+
 
 }
 
