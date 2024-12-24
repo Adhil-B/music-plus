93f34a09020096542bbe7e51cf19a3d2f793b9db
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-12-25 05:15:21 +0530

    Update Track.jsx

--- src/components/MusicPlayer/Track.jsx
+++ src/components/MusicPlayer/Track.jsx
@@ -5,7 +5,7 @@ const Track = ({ isPlaying, isActive, activeSong, fullScreen }) => (
     <div className={`${isPlaying && isActive ? 'animate-[spin_15s_linear_infinite]' : ''} hidden sm:block ml-4 h-16 w-16`}>
       <img src={activeSong?.image?.[2].link || 'https://musicplus-web.vercel.app/Music.png'} alt="cover art" className="h-[100%] object-cover rounded-full"/>
     </div>
-    <div className={`w-[190px] ml-4 select-none cursor-pointer`}>
+    <div className={`w-[190px] ${fullScreen ? 'ml-4' : 'ml-[10px] sm:ml-4'} select-none cursor-pointer`}>
       <p className="truncate text-white font-bold text-lg">
         {activeSong?.name ? activeSong?.name.replace("&#039;", "'").replace("&amp;", "&").replaceAll('&quot;','"') : 'Song'}
       </p>
