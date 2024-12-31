96f263285ba84eea325831903b7df83ecfdc4f51
Adhil <75608493+Adhil-B@users.noreply.github.com>
2024-12-31 13:35:48 +0530

    Update Searchbar.jsx

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,5 @@
  'use client'
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import { useRouter } from 'next/navigation';
 import { useDispatch } from 'react-redux';
