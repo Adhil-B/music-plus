eb159bde87a619a27134eeefe5ff783c0563ef83
Adhil <75608493+Adhil-B@users.noreply.github.com>
2025-01-06 02:01:58 +0530

    Update Searchbar.jsx

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -116,7 +116,7 @@ const Searchbar = () => {
           className="fixed w-[0px] top-[0px] flex-1 bg-transparent focus:border-b border-white h-[0px] placeholder-gray-300 outline-none text-base text-white"
           placeholder=""
           ref={inputRef}
-          type="hidden"
+          type={`${isTyping ? 'hidden':''}`}
           value=''
         />
 
