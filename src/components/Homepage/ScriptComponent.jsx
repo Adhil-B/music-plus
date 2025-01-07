3f5db57d38eb444a84e503dee7b87a11b55870e6
Adhil <75608493+Adhil-B@users.noreply.github.com>
2025-01-08 02:21:35 +0530

    Update ScriptComponent.jsx

--- src/components/Homepage/ScriptComponent.jsx
+++ src/components/Homepage/ScriptComponent.jsx
@@ -2,25 +2,38 @@ import React, { useEffect } from 'react';
 
 const ScriptComponent = () => {
   useEffect(() => {
+    // Dynamically create the script element
     const script = document.createElement('script');
-    const l = document.scripts[document.scripts.length - 1];
+    const container = document.getElementById('ad-container');
 
-    script.settings = {}; // Equivalent to the `upszm` argument in the function
-    script.src = "//infantilecombination.com/bGX_V.stdTGVlP0CYEWedti-YlWD5FuYZAXlIQ/Mecm/9lu/ZAUJlCkZPrTcUq5eOTTAcB3-NhDAg/taNPTgkd5CNDzqc/0/OQQw";
-    script.async = true;
-    script.referrerPolicy = 'no-referrer-when-downgrade';
+    script.innerHTML = `
+      (function(upszm){
+        var d = document,
+            s = d.createElement('script'),
+            l = d.scripts[d.scripts.length - 1];
+        s.settings = upszm || {};
+        s.src = "//infantilecombination.com/bGX_V.stdTGVlP0CYEWedti-YlWD5FuYZAXlIQ/Mecm/9lu/ZAUJlCkZPrTcUq5eOTTAcB3-NhDAg/taNPTgkd5CNDzqc/0/OQQw";
+        s.async = true;
+        s.referrerPolicy = 'no-referrer-when-downgrade';
+        l.parentNode.insertBefore(s, l);
+      })({});
+    `;
 
-    l.parentNode.insertBefore(script, l);
+    container.appendChild(script);
 
-    // Cleanup function to remove the script if necessary
+    // Cleanup function to remove the script if the component unmounts
     return () => {
-      if (script.parentNode) {
-        script.parentNode.removeChild(script);
+      while (container.firstChild) {
+        container.removeChild(container.firstChild);
       }
     };
   }, []);
 
-  return <div>Script loaded</div>;
+  return (
+    <div id="ad-container">
+      {/* Placeholder for the ad */}
+    </div>
+  );
 };
 
 export default ScriptComponent;
