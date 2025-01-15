abddea1fb8ed1d628f601ad24b2fa27321a09251
Adhil Bijukumar <75608493+Adhil-B@users.noreply.github.com>
2025-01-15 23:11:02 +0400

    fix

--- src/app/AdBanner.jsx
+++ src/app/AdBanner.jsx
@@ -20,12 +20,7 @@ useEffect(() => {
 
   
   return (
-
-    <div className='flex sm:hidden justify-center rounded-lg bg-[hsla(0,0%,100%,.05)] p-[10px] sm:p-[0px] mb-[15px] min-h-[60px]' id={`ad-container-full`} >
-    <div className='absolute z-[-1]'>
-    <center>Advertisement</center>
-    </div>
-    </div>
+    <div id="ad-banner"></div>
   );
 };
 
